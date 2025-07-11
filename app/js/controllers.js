0f172630fca50fb9221342242e4981cd770da275
Tom J Nowell <contact@tomjn.com>
2025-07-11 21:02:07 +0100

    Move where decks are defined into scope so they can be read from partials

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,22 @@
 'use strict';
 
 /* Controllers */
-
 function MainCtrl($scope, $timeout) {
   $scope.logoState = '';
   $scope.errorMessage = null;
   $scope.message = null;
+	$scope.decks = {
+		'135 set': [ '1', '3', '5', '8', '13', '21', '?'],
+		'Fibonacci': ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '?'],
+		'Fibonacci Goat': [ '1', '2', '3', '5', '8', '13', '?', '\u2615' ],
+		'Mountain Goat': ['0', '\u00BD', '1', '2', '3', '5', '8', '13', '20', '40', '100', '?', '\u2615'],
+		'Sequential': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'],
+		'Playing Cards': ['A\u2660', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J\u2654', 'Q\u2654', 'K\u2654'],
+		'T-Shirt': ['XL', 'L', 'M', 'S', 'XS', '?'],
+		'Fruit': ['🍎', '🍊', '🍌', '🍉', '🍑', '🍇'],
+		'1-5': [1, 2, 3, 4, 5],
+		'1-10': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	};
 
   $scope.$on('$routeChangeSuccess', function () {
     $scope.logoState = '';
@@ -203,21 +214,8 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
   };
 
   var chooseCardPack = function (val) {
-    var decks = {
-      '135 set': [ '1', '3', '5', '8', '13', '21', '?'],
-      'Fibonacci': ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '?'],
-      'Fibonacci Goat': [ '1', '2', '3', '5', '8', '13', '?', '\u2615' ],
-      'Mountain Goat': ['0', '\u00BD', '1', '2', '3', '5', '8', '13', '20', '40', '100', '?', '\u2615'],
-      'Sequential': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'],
-      'Playing Cards': ['A\u2660', '2', '3', '5', '8', '\u2654'],
-      'T-Shirt': ['XL', 'L', 'M', 'S', 'XS', '?'],
-      'Fruit': ['🍎', '🍊', '🍌', '🍉', '🍑', '🍇'],
-      '1-5': [1, 2, 3, 4, 5],
-      '1-10': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    };
-
-		if ( val in decks ) {
-      return decks[val];
+		if ( val in $scope.decks ) {
+      return $scope.decks[val];
     }
 
     return val.split( ',' );
