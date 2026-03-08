28c7fb67ae785275eb93f23ee17ff9689778323f
Tom J Nowell <contact@tomjn.com>
2026-03-08 15:14:49 +0000

    Fix logger to resolve printf-style format specifiers
    Use util.format() so %d/%s placeholders in the message string are
    substituted before the log-level prefix is prepended.

--- server/logger.js
+++ server/logger.js
@@ -1,12 +1,13 @@
+const { format } = require('util');
 const env = process.env.NODE_ENV || 'development';
 
 const logger = {
-  info: (...args) => console.log('[INFO]', ...args),
-  warn: (...args) => console.warn('[WARN]', ...args),
-  error: (...args) => console.error('[ERROR]', ...args),
+  info: (...args) => console.log('[INFO]', format(...args)),
+  warn: (...args) => console.warn('[WARN]', format(...args)),
+  error: (...args) => console.error('[ERROR]', format(...args)),
   debug: (...args) => {
     if (env !== 'production') {
-      console.log('[DEBUG]', ...args);
+      console.log('[DEBUG]', format(...args));
     }
   }
 };
